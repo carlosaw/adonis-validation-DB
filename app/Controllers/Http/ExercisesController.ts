8755cf816eef70b7be85e65179a97f6b1bc7b89f
carlosaw <carlosfreevg@gmail.com>
2023-06-28 21:02:28 -0400

    Project finish

--- app/Controllers/Http/ExercisesController.ts
+++ app/Controllers/Http/ExercisesController.ts
@@ -3,19 +3,19 @@ import { schema, rules } from '@ioc:Adonis/Core/Validator'
 
 export default class ExercisesController {
   /*
-   *  username e  password são obrigatórios
-   *  a idade (age) do usuário deve ser um number e não pode ser menor que 18
-   *  password deve ter entre 6 e 12 caracteres
-   *  sex deve aceitar somente tipo M e F
+   *1- username e  password são obrigatórios
+   *2- password deve ter entre 6 e 12 caracteres
+   *3- a idade (age) do usuário deve ser um number e não pode ser menor que 18
+   *4- sex deve aceitar somente tipo M e F
    */
   public async validateN1(ctx: HttpContextContract) {
     const { request } = ctx
 
     const exerciseSchema = schema.create({
-      username: schema.string(),
-      password: schema.string([rules.minLength(6), rules.maxLength(12)]),
-      age: schema.number([rules.range(18, 150)]),
-      sex: schema.enum(['M', 'F'] as const),
+/*1*/ username: schema.string(),
+/*2*/ password: schema.string([rules.minLength(6), rules.maxLength(12)]),
+/*3*/ age: schema.number([rules.range(18, 150)]),
+/*4*/ sex: schema.enum(['M', 'F'] as const),          
     })
 
     const validated = await request.validate({
@@ -26,22 +26,22 @@ export default class ExercisesController {
   }
 
   /*
-   * username e  password são obrigatórios
-   * o usuário tem que ter 18 anos ou mais (birthdate)
-   * password deve ter entre 6 e 12 caracteres
-   * sex deve aceitar somente tipo M e F
-   * isAdmin deve aceitar somente os valores true ou false
+   *1- username e  password são obrigatórios
+   *2- o usuário tem que ter 18 anos ou mais (birthdate)
+   *3- password deve ter entre 6 e 12 caracteres
+   *4- sex deve aceitar somente tipo M e F
+   *5- isAdmin deve aceitar somente os valores true ou false
    */
   public async validateN2(ctx: HttpContextContract) {
     const { request } = ctx
 
     const exerciseSchema = schema.create({
-      username: schema.string(),
-      password: schema.string([rules.minLength(6), rules.maxLength(12)]),
-      birthdate: schema.date({format: "dd/MM/yyyy"}, [rules.before(18, 'years')]),
-      sex: schema.enum(['M', 'F'] as const),
-      isAdmin: schema.boolean(),
-      rules: schema.array().members(schema.number([rules.notIn([2])])),
+/*1*/ username: schema.string(),
+/*2*/ password: schema.string([rules.minLength(6), rules.maxLength(12)]),
+/*3*/ birthdate: schema.date({format: "dd/MM/yyyy"}, [rules.before(18, 'years')]),
+/*4*/ sex: schema.enum(['M', 'F'] as const),
+/*5*/ isAdmin: schema.boolean(),
+/*6*/ rules: schema.array().members(schema.number([rules.notIn([2])])),
     })
 
     const validated = await request.validate({
@@ -52,32 +52,34 @@ export default class ExercisesController {
   }
 
   /**
-   * carrinho de compras chamado cart com pedidos de uma loja virtual
-   * cada pedido deve ter pelo menos um item associado
-   * o prazo de entrega informado dev ser de pelo menos cinco dias após a data do pedido
-   * o pedido dever ter um endereço de entrega cadastrado
-   * o pedido deve ter um usuário associado
-   * cada item deve ter um id do tipo number
-   * o ip do usuário de ve ser salvo no pedido
+   *1- carrinho de compras chamado cart com pedidos de uma loja virtual.
+   *2- cada carrinho deve ter pelo menos um item associado.
+   *3- cada item deve ter um id do tipo number.
+   *4- o prazo de entrega informado deve ser de pelo menos cinco dias após a data do pedido.
+   *5- o carrinho dever ter um endereço de entrega cadastrado.
+   *6- o carrinho deve ter um usuário associado.
+   
+   *7- o ip do usuário deve ser salvo no carrinho.
    */
   public async validateN3(ctx: HttpContextContract) {
     const { request } = ctx
 
     const exerciseSchema = schema.create({
-      cart: schema.object().members({
-        orders: schema.array().members(
+/*1*/ cart: schema.object().members({
+/*2*/   orders: schema.array().members(
           schema.object().members({
-            id: schema.enum([1, 2, 3, 4, 7, 12, 122] as const),
+/*3*/       id: schema.number(),
+/*3*/     //id: schema.enum([1, 2, 3, 4, 7, 12, 122] as const),
             quantity: schema.number(),
           })
         ),
-        delivery: schema.date({format: "dd/MM/yyyy"}, [rules.before(-5, 'days')]),
-        address: schema.object().members({
+/*4*/ delivery: schema.date({format: "dd/MM/yyyy HH:mm:ss"}, [rules.before(-5, 'days')]),
+/*5*/   address: schema.object().members({
           city: schema.string(),
           state: schema.string(),
         }),
-        user: schema.object().members({
-          ip: schema.string([rules.ip()]),
+/*6*/   user: schema.object().members({
+/*7*/     ip: schema.string([rules.ip()]),
           name: schema.string(),
         }),
       }),
